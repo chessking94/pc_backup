35a63584dec85b0a502d85d26d115acc8b8a3583
Ethan Hunt <chessking94@gmail.com>
2021-07-20 21:44:41 -0500

    Initial script

---
+++ backup_files.bat
@@ -0,0 +1,5 @@
+robocopy "C:\Users\eehunt\Documents" "D:\eehunt\Documents" /e /xo /r:0 /w:0
+robocopy "C:\Users\eehunt\Music" "D:\eehunt\Music" /e /xo /r:0 /w:0
+robocopy "C:\Users\eehunt\Pictures" "D:\eehunt\Pictures" /e /xo /r:0 /w:0
+robocopy "C:\FileProcessing" "D:\System\FileProcessing" /e /mov /r:0 /w:0
+robocopy "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup" "D:\System\MSSQLSERVER\DATA" /e /xo /r:0 /w:0
