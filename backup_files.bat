0e603e5e588fdbab2e02ea45d668d1050cee61e9
Ethan Hunt <chessking94@gmail.com>
2021-09-15 21:42:57 -0500

    Added backup of scheduled scripts

--- backup_files.bat
+++ backup_files.bat
@@ -4,3 +4,4 @@ robocopy "C:\Users\eehunt\Pictures" "D:\eehunt\Pictures" /e /xo /r:0 /w:0
 robocopy "C:\FileProcessing" "D:\System\FileProcessing" /e /mov /r:0 /w:0
 robocopy "C:\Users\eehunt\Repository" "D:\eehunt\Repository" /mir /r:0 /w:0
 robocopy "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup" "D:\System\MSSQLSERVER\DATA" /e /xo /r:0 /w:0
+robocopy "C:\Users\eehunt\Desktop\scheduled scripts" "D:\System\Scripts" /mir /r:0 /w:0
