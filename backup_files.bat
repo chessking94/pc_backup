2a71e57827dd1284e26887be2f0172988bb95642
Ethan Hunt <chessking94@gmail.com>
2021-11-27 20:01:04 -0600

    Inclusion of cloud backups

--- backup_files.bat
+++ backup_files.bat
@@ -1,3 +1,4 @@
+:: Secondary hard drive
 robocopy "C:\Users\eehunt\Documents" "D:\eehunt\Documents" /e /xo /r:0 /w:0
 robocopy "C:\Users\eehunt\Music" "D:\eehunt\Music" /e /xo /r:0 /w:0
 robocopy "C:\Users\eehunt\Pictures" "D:\eehunt\Pictures" /e /xo /r:0 /w:0
@@ -5,5 +6,15 @@ robocopy "C:\FileProcessing" "D:\System\FileProcessing" /e /mov /r:0 /w:0
 robocopy "C:\Users\eehunt\Repository" "D:\eehunt\Repository" /mir /r:0 /w:0
 robocopy "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup" "D:\System\MSSQLSERVER\DATA" /e /xo /r:0 /w:0
 robocopy "C:\Users\eehunt\Desktop\scheduled scripts" "D:\System\Scripts" /mir /r:0 /w:0
+:: Google Drive
 copy /b "C:\Users\eehunt\Documents\Chess\Chess Game Statistics v3.xlsx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
 copy /b "C:\Users\eehunt\Documents\Chess\Online Chess Game Statistics.xlsx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
+copy /b "C:\Users\eehunt\Documents\Chess\UsernameDownloadSelections.xlsm" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
+copy /b "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\ChessAnalysis.bak" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
+robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "LiveOnline.*"
+robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "CCGamesOnline.*"
+robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "ChessCastle.*"
+robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "Correspondence.*"
+robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "EthanHuntGames*"
+robocopy "C:\Users\eehunt\Documents\ChessBase\Books" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "Ethan*"
+:: Onedrive
