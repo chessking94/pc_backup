f410b7978348dd8bcd931f958f3553f6921d2685
Ethan Hunt <chessking94@gmail.com>
2022-03-22 21:11:58 -0500

    Back up those valuable prep files!

--- backup_files.bat
+++ backup_files.bat
@@ -16,5 +16,6 @@ robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRI
 robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "ChessCastle.*"
 robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "Correspondence.*"
 robocopy "C:\Users\eehunt\Documents\ChessBase\Bases" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "EthanHuntGames*"
+robocopy "C:\Users\eehunt\Documents\ChessBase\Bases\Preparation" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "PlayerPrep*"
 robocopy "C:\Users\eehunt\Documents\ChessBase\Books" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess\BACKUP" "Ethan*"
 :: Onedrive
