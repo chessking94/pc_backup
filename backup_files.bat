73abe379127375ea6803d4e0a03fb9b64031cedd
Ethan Hunt <chessking94@gmail.com>
2022-11-05 06:45:02 -0500

    Switch to newest iteration of chess statistics

--- backup_files.bat
+++ backup_files.bat
@@ -7,7 +7,7 @@ robocopy "C:\Users\eehunt\Repository" "D:\eehunt\Repository" /mir /r:0 /w:0
 robocopy "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup" "D:\System\MSSQLSERVER\DATA" /mir /r:0 /w:0
 robocopy "C:\Users\eehunt\Desktop\scheduled scripts" "D:\System\Scripts" /mir /r:0 /w:0
 :: Google Drive
-copy /b "C:\Users\eehunt\Documents\Chess\Chess Game Statistics v3.xlsx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
+copy /b "C:\Users\eehunt\Documents\Chess\Chess Game Statistics v4.xlsx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
 copy /b "C:\Users\eehunt\Documents\Chess\Online Chess Game Statistics.xlsx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
 copy /b "C:\Users\eehunt\Documents\Chess\UsernameDownloadSelections.xlsm" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
 copy /b "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\ChessWarehouse.bak" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
