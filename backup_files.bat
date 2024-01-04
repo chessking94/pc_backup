8299edead7338d68024821fc4b670cf6a9b27fde
Ethan Hunt <chessking94@gmail.com>
2024-01-03 21:59:24 -0600

    Old commit from switching from D drive to S

--- backup_files.bat
+++ backup_files.bat
@@ -1,12 +1,13 @@
 :: Secondary hard drive
-robocopy "C:\Users\eehunt\Documents" "D:\eehunt\Documents" /mir /r:0 /w:0
-robocopy "C:\Users\eehunt\Music" "D:\eehunt\Music" /e /xo /r:0 /w:0
-robocopy "C:\Users\eehunt\Pictures" "D:\eehunt\Pictures" /e /xo /r:0 /w:0
-robocopy "C:\FileProcessing" "D:\System\FileProcessing" /e /mov /r:0 /w:0
-robocopy "C:\Users\eehunt\Repository" "D:\eehunt\Repository" /mir /r:0 /w:0
-robocopy "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup" "D:\System\MSSQLSERVER\DATA" /mir /r:0 /w:0
-robocopy "C:\Users\eehunt\Desktop\scheduled scripts" "D:\System\Scripts" /mir /r:0 /w:0
+robocopy "C:\Users\eehunt\Documents" "S:\eehunt\Documents" /mir /r:0 /w:0
+robocopy "C:\Users\eehunt\Music" "S:\eehunt\Music" /e /xo /r:0 /w:0
+robocopy "C:\Users\eehunt\Pictures" "S:\eehunt\Pictures" /e /xo /r:0 /w:0
+robocopy "C:\FileProcessing" "S:\System\FileProcessing" /e /mov /r:0 /w:0
+robocopy "C:\Users\eehunt\Repository" "S:\eehunt\Repository" /mir /r:0 /w:0
+robocopy "C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup" "S:\System\MSSQLSERVER\DATA" /mir /r:0 /w:0
+robocopy "C:\Users\eehunt\Desktop\scheduled scripts" "S:\System\Scripts" /mir /r:0 /w:0
 :: Google Drive
+copy /b "C:\Users\eehunt\Documents\Domain\Local Domain.kdbx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Misc"
 copy /b "C:\Users\eehunt\Documents\Chess\Chess Game Statistics v4.xlsx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
 copy /b "C:\Users\eehunt\Documents\Chess\Online Chess Game Statistics v2.xlsx" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
 copy /b "C:\Users\eehunt\Documents\Chess\UsernameDownloadSelections.xlsm" "C:\Users\eehunt\GOOGLE_DRIVE_BACKUP\Chess"
